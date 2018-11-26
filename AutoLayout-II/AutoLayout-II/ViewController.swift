4a2c4f422f883921c935646ed8c966179352f6b4
jkaunert <jkaunert@clusterfly.net>
2018-11-26 15:45:51 -0600

    initial commit. adds xibs and imagesets.

---
+++ AutoLayout-II/AutoLayout-II/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout-II
+//
+//  Created by TuneUp Shop  on 11/26/18.
+//  Copyright © 2018 jkaunert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
