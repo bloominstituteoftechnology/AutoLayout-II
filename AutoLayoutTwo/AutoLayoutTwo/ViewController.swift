3662b1d67d53d8041b24464766a713efdd92523c
Jonah Bergevin <jonahbergevin@gmail.com>
2018-11-26 15:14:14 -0700

    Answer questions in Read Me file submitted

---
+++ AutoLayoutTwo/AutoLayoutTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutTwo
+//
+//  Created by Madison Waters on 11/26/18.
+//  Copyright © 2018 Jonah Bergevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
