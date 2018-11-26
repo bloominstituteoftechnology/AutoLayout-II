29bfd55cc73d406470c4b3b89cd0587ddc345eeb
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2018-11-26 17:29:33 -0500

    add new view files to complete challenges

---
+++ AutoLayoutII/AutoLayoutII/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutII
+//
+//  Created by Julian A. Fordyce on 11/26/18.
+//  Copyright © 2018 Julian A. Fordyce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
